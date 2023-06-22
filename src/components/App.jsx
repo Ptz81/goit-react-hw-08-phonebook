321185ea255187fc23722315f66c0aa645ed3ec5
Taras Papka <taras.papka@protonmail.com>
2023-06-22 10:09:50 +0300

    added layout and modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout/Layout';
-import { Homepage } from './Homepage/Homepage';
-import { Options } from './Options/Options';
 import { SignUpPage } from './SignUpPage/SignUpPage';
 import { LoginPage } from './LoginPage/LoginPage';
+import Layout from './Layout/Layout';
+import { Homepage } from './Homepage/Homepage';
+import { Options } from './Options/Options';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Layout/>}>
         <Route index element={<Homepage/>}/>
         <Route path='/'element={<Options/>}/>
       </Route>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,39 @@
-export const Layout = () => {
-  
+import React, { Suspense, useState } from 'react'
+
+import { Outlet } from 'react-router-dom'
+import Modal from '../Modal/Modal'
+
+import { Header } from 'components/Header/Header'
+import { FormLogin } from 'components/FormLogin/FormLogin'
+
+const Layout = () => {
+	const [isShowModal, setIsShowModal] = useState(false)
+
+	const openModal = () => setIsShowModal(true)
+
+	const closeModal = () => setIsShowModal(false)
+
+	const createUser = (data) => {
+		const newUser = {
+			...data,
+			id: Date.now(),
+			role: 'customer',
+		}
+		console.log(newUser)
+	}
+	return (
+		<div className='container'>
+			<Header open={openModal} />
+			<Suspense>
+				<Outlet />
+			</Suspense>
+			{isShowModal && (
+				<Modal close={closeModal}>
+					<FormLogin close={closeModal} createUser={createUser} />
+				</Modal>
+			)}
+		</div>
+	)
 }
+
+export default Layout
