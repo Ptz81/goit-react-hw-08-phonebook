1af95bb6c2edfed4ae1f41954767e37f84892cf3
Taras Papka <taras.papka@protonmail.com>
2023-06-28 11:00:15 +0300

    added filter

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet';
 import { selectLoading } from 'redux/tasks/selectors';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+import css from './pages.module.css'
 
 
 export default function Contacts() {
@@ -11,12 +13,29 @@ export default function Contacts() {
 
   return (
     <>
-      <Helmet>
+      <div className={css.contactPage}>
+        <Helmet>
         <title>Your Contacts</title>
       </Helmet>
-      <ContactForm />
-      <div>{isLoading && 'Request in progress...'}</div>
+        <ContactForm />
+        <div>{isLoading && 'Request in progress...'}</div>
+         <div className={css.contact_container}>
+         <div className={css.subcontainer}>
       <ContactList />
+        </div>
+        <div className={css.subcontainer}>
+      <Helmet>
+        <title>Your Contact's filter</title>
+      </Helmet>
+      <div>{isLoading && 'Request in progress...'}</div>
+      <Filter />
+      </div>
+
+      </div>
+</div>
+
+
+
     </>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import css from './pages.module.css'
 export default function Home() {
   return (
-    <div className={css.container}>
+    <div className={css.home}>
       <h1 className={css.title}>
         Welcome to Contact Book page!
       </h1>
